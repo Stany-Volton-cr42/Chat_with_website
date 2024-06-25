fce002099cfac5fa6ba111af26b497c31c85899e
Vivek raj <vr559081@gmail.com>
2024-06-25 13:08:47 +0530

    next commit

--- src/app.py
+++ src/app.py
@@ -1,20 +1,53 @@
 # import the streamlit
 
 import streamlit as st
-st.set_page_config(page_title="Chat-with-website",page_icon="💬")
+from langchain_core.messages import AIMessage, HumanMessage
+
+
+def get_response(user_query):
+    return "First, please build me"
+
 
 # For the interface
+st.set_page_config(page_title="Chat-with-website",page_icon="💬")
 st.title("Chat with website")
+
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history =[
+    AIMessage(content= "Hello, I am a bot. How can I help you")
+
+]
+    
+
+
+# app Sidebar
 with st.sidebar:
     st.header("Settings")
     website_URL = st.text_input("Website url")
 
-st.chat_input("Type your message here... ")
 
-with st.chat_message("AI"):
-    st.write("Hello! how can I help you ")
+#   CHAT HISTORY / AI response to User Input
+user_query = st.chat_input("Type your message here... ")
+if user_query is not None and user_query != "":
+    response = get_response(user_query)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
 
-with st.chat_message("Human"):
-    st.write("I want to know about the website")
+# with st.sidebar:
+#     st.write(st.session_state.chat_history)
+
+# conversation
+
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message,HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+
+
 
 
